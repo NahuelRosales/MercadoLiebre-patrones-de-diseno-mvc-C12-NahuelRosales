1fa9122096f505521814c5ee2019147674690ee3
Nahuel Rosales <nahuelrosales@outlook.com>
2022-04-04 03:16:12 -0300

    primer commit

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require ("express")
+const path = require ("path")
+const app = express()
+const PORT = 3030
+
+const publicPath = path.resolve(__dirname, "./public");
+app.use(express.static(publicPath));
+
+app.listen (PORT, () => {
+    console.log(`Servidor levantado en el puerto ${PORT}
+    http://localhost:${PORT}
+    `);
+});
+
+app.get("/", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./views/home.html"))
+})
+app.get("/register", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./views/register.html"))
+})
+app.get("/login", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./views/login.html"))
+})
